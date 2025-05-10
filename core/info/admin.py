cfd48ad06bce5beae320b634b6cee6b192482971
Shahriar Gholami <shahriar.gholami.mehrabadi@gmail.com>
2025-05-10 14:02:03 +0430

    first version done

--- core/info/admin.py
+++ core/info/admin.py
@@ -1,3 +1,74 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Row)
+class RowAdmin(admin.ModelAdmin):
+    list_display = ('title', )
+
+@admin.register(RackType)
+class RackTypeAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+@admin.register(Rack)
+class RackAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'type', 'row')
+    list_filter = ('type', 'row')
+    search_fields = ('name',)
+
+@admin.register(DeviceType)
+class DeviceTypeAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'device_type', 'series', 'rack')
+    list_filter = ('device_type', 'rack')
+    search_fields = ('name', 'series')
+
+
+
+
+@admin.register(Interface)
+class InterfaceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'device', 'get_rack', 'get_row', 'get_pathes_display')
+    list_filter = ('device','device__rack', 'device__rack__row')
+    search_fields = ('name','device__name')
+
+    def get_pathes_display(self, obj):
+        pathes = obj.get_pathes()
+        return ", ".join(str(t) for t in pathes)
+
+    get_pathes_display.short_description = "Path"
+
+    def get_rack(self, obj):
+        return obj.device.rack
+    get_rack.short_description = 'Rack'
+
+    def get_row(self, obj):
+        return obj.device.rack.row if obj.device and obj.device.rack else None
+    get_row.short_description = 'Row'
+
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('id', 'path_position', 'get_terminals_display')
+    filter_horizontal = ('terminals',)
+    search_fields = ('terminals__name', )
+
+    def get_terminals_display(self, obj):
+        return ", ".join(str(t) for t in obj.terminals.all())
+    get_terminals_display.short_description = "Terminals"
+
+@admin.register(Path)
+class PathAdmin(admin.ModelAdmin):
+    list_display = ('id', 'get_links_count', 'get_terminals_display')
+    filter_horizontal = ('links',)
+
+    def get_links_count(self, obj):
+        return obj.links.count()
+    get_links_count.short_description = "Links Count"
+
+    def get_terminals_display(self, obj):
+        terminals = obj.get_terminals()
+        return ", ".join(str(t) for t in terminals)
+    get_terminals_display.short_description = "Terminals"
--- core/info/models.py
+++ core/info/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import m2m_changed
+from django.dispatch import receiver
 
 # Create your models here.
 
@@ -20,10 +22,10 @@ class Rack(models.Model):
     row = models.ForeignKey(Row, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
-        return f'{self.row}-{self.type}-{self.name}'
+        return f'{self.row}-{self.name}'
 
 class DeviceType(models.Model):
-    title = models.CharField
+    title = models.CharField(max_length=255)
 
     def __str__(self) -> str:
         return self.title
@@ -41,6 +43,16 @@ class Interface(models.Model):
     name = models.CharField(max_length=255)
     device = models.ForeignKey(Device, on_delete=models.CASCADE)
 
+    def get_pathes(self):
+        pathes = []
+        for path in Path.objects.all():
+            path_terminals = path.get_terminals()
+            if self in path_terminals:
+                pathes.append(path)
+
+        return pathes
+
+
     def __str__(self) -> str:
         return f'{self.device}-{self.name}'
 
@@ -48,44 +60,50 @@ class Link(models.Model):
     terminals = models.ManyToManyField(Interface)
     path_position = models.IntegerField(default=1)
 
-    def save(self, *args, **kwargs):
+    def __str__(self) -> str:
+        return ' | '.join(str(terminal) for terminal in self.terminals.all())
 
-        from .models import Path  # جلوگیری از ImportError چرخشی
-        selected_path = None
-        
+@receiver(m2m_changed, sender=Link.terminals.through)
+def handle_terminals_change(sender, instance, action, **kwargs):
+    if action == 'post_add':
+        # فقط وقتی ترمینال‌ها اضافه شدن
+        pathes = []
         for path in Path.objects.all():
+            path_terminals = path.get_terminals()
+            for terminal in instance.terminals.all():
+                if terminal in path_terminals:
+                    pathes.append(path)
+                    break
+            if len(pathes) == 2:
+                break
 
-            if path.get_end_interface() == self.source_int:
-                path.links.add(self)
-                print(f'به انتهای مسیر {path.id} اضافه شد')
-                selected_path = path
+        if len(pathes) == 0:
+            print('00000000000000000000000000000000000000')
+            new_path = Path.objects.create()
+            new_path.links.add(instance)
+        elif len(pathes) == 1:
+            print('111111111111111111111111111111111')
+            pathes[0].links.add(instance)
+        elif len(pathes) == 2:
+            print('22222222222222222222222222222222')
+            for link in pathes[1].links.all():
+                pathes[0].links.add(link)
+            pathes[1].delete()
+
+
 
 
 class Path(models.Model):
     links = models.ManyToManyField(Link, blank=True)
 
-    def get_start_interface(self):
-        if not self.links.exists():
-            return None
-        first_link = self.links.first()
-        path_terminals = []
-        return first_link.source_int
+    def get_terminals(self):
+        path_terminals = set()
+        for link in self.links.all():
+            for terminal in link.terminals.all():
+                path_terminals.add(terminal)
+        return path_terminals
 
-    def get_end_interface(self):
-        if not self.links.exists():
-            return None
-        last_link = self.links.last()
-        return last_link.destination_int
+    def __str__(self) -> str:
 
-    def can_append(self, link: Link):
-        return self.get_end_interface() == link.source_int
-
-    def can_prepend(self, link: Link):
-        return self.get_start_interface() == link.destination_int
-
-    
-
-    
-    
-
+        return ' || '.join(str(link) for link in self.links.all())
 
